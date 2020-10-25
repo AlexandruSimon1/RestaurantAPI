dca2c9377c88d6e443b9e4221b32e0d866138c57
Alexandru Simon <simonsanea@gmail.com>
2020-10-25 10:05:15 +0200

    Adding the components for our REST endpoints

--- restaurant-ui/src/app/app.module.ts
+++ restaurant-ui/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministratorsComponent } from './administrators/administrators.component';
+import { WaitersComponent } from './waiters/waiters.component';
+import { OrdersComponent } from './orders/orders.component';
+import { MenusComponent } from './menus/menus.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { TablesComponent } from './tables/tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministratorsComponent,
+    WaitersComponent,
+    OrdersComponent,
+    MenusComponent,
+    CheckoutComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
