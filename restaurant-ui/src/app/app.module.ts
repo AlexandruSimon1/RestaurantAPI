93dd5b0352aa1255bf9f0824820b794161984026
Arthur_Simon <asimon@ms3-inc.com>
2020-11-27 16:43:18 +0200

    Created components for all SQL tables that we are gone use

--- restaurant-ui/src/app/app.module.ts
+++ restaurant-ui/src/app/app.module.ts
@@ -16,6 +16,20 @@ import { CreateAdministratorComponent } from './modules/administrator/create-adm
 import { CreateWaiterComponent } from './modules/waiter/create-waiter/create-waiter.component';
 import { UpdateAdministratorComponent } from './modules/administrator/update-administrator/update-administrator.component';
 import { UpdateWaiterComponent } from './modules/waiter/update-waiter/update-waiter.component';
+import { AdministratorDetailComponent } from './modules/administrator/administrator-detail/administrator-detail.component';
+import { WaiterDetailComponent } from './modules/waiter/waiter-detail/waiter-detail.component';
+import { OrderDetailComponent } from './modules/order/order-detail/order-detail.component';
+import { CheckoutDetailComponent } from './modules/checkout/checkout-detail/checkout-detail.component';
+import { MenuDetailComponent } from './modules/menus/menu-detail/menu-detail.component';
+import { TablesDetailComponent } from './modules/table/tables-detail/tables-detail.component';
+import { UpdateTableComponent } from './modules/table/update-table/update-table.component';
+import { UpdateOrderComponent } from './modules/order/update-order/update-order.component';
+import { UpdateMenusComponent } from './modules/menus/update-menus/update-menus.component';
+import { UpdateCheckoutComponent } from './modules/checkout/update-checkout/update-checkout.component';
+import { CreateCheckoutComponent } from './modules/checkout/create-checkout/create-checkout.component';
+import { CreateMenusComponent } from './modules/menus/create-menus/create-menus.component';
+import { CreateOrderComponent } from './modules/order/create-order/create-order.component';
+import { CreateTablesComponent } from './modules/table/create-tables/create-tables.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +43,21 @@ import { UpdateWaiterComponent } from './modules/waiter/update-waiter/update-wai
     TableComponent,
     CheckoutComponent,
     CreateWaiterComponent,
-    UpdateWaiterComponent
+    UpdateWaiterComponent,
+    AdministratorDetailComponent,
+    WaiterDetailComponent,
+    OrderDetailComponent,
+    CheckoutDetailComponent,
+    MenuDetailComponent,
+    TablesDetailComponent,
+    UpdateTableComponent,
+    UpdateOrderComponent,
+    UpdateMenusComponent,
+    UpdateCheckoutComponent,
+    CreateCheckoutComponent,
+    CreateMenusComponent,
+    CreateOrderComponent,
+    CreateTablesComponent
   ],
   imports: [
     BrowserModule,
