3e2041ecd52f35bd695b6f4fb19112a6f6741beb
Arthur_Simon <asimon@ms3-inc.com>
2022-01-20 16:49:21 +0200

    Testing domain options

--- restaurant-ui/src/environments/environment.prod.ts
+++ restaurant-ui/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  env: 'LOCAL',
+  baseAPIUrl: 'http://ec2-3-70-243-119.eu-central-1.compute.amazonaws.com:80/api/v1',
+  api: {
+    administrator: 'administrators',
+    waiter: 'waiters',
+    menu: 'menus',
+    order: 'orders',
+    table: 'tables',
+    checkout: 'checkout',
+  },
 };
